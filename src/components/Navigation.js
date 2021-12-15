360247abc14a8e49b4e45643cad2fc61c1b8cd66
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-12-15 09:43:14 +0100

    Add skeleton for skill button component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,16 +2,7 @@ import React from 'react';
 
 function Navigation() {
   return (
-    <div>
-      <p>Language</p>
-      <p>Search</p>
-      <p>Job/Gigs</p>
-      <p>Post a job</p>
-      <p>Your jobs</p>
-      <p>Preferences</p>
-      <p>Genome</p>
-
-    </div>
+    <></>
   );
 }
 
---
+++ src/components/SkillButon.js
@@ -0,0 +1,18 @@
+import React, { } from 'react';
+
+function skillButton(buttonDetails) {
+  const {id, name,} = buttonDetails
+
+  const getSkillDetails = (e) => {
+    const skillId = e.target.id
+  }
+
+
+  return (
+    <button id={id} onSubmit={getSkillDetails}>
+      {name}
+    </button>
+  )
+}
+
+export default skillButton
